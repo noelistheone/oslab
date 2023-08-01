2581349bd85e5c4474e31966267f7fcd1bb2f708
noelistheone <1781680419@qq.com>
2023-08-01 21:32:38 +0800

    ready to submit my lab

--- kernel/trap.c
+++ kernel/trap.c
@@ -75,6 +75,14 @@ usertrap(void)
 
   if(p->killed)
     exit(-1);
+  if(which_dev == 2){   // timer interrupt
+    // increase the passed ticks
+    if(p->interval != 0 && ++p->passedticks == p->interval){
+      p->trapframecopy = p->trapframe + 512;  
+      memmove(p->trapframecopy,p->trapframe,sizeof(struct trapframe));
+      p->trapframe->epc = p->handler;   // execute handler() when return to user space 
+    }
+  }
 
   // give up the CPU if this is a timer interrupt.
   if(which_dev == 2)
